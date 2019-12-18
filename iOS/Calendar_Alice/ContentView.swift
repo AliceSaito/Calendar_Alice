66ee59e1af2c367776558820e73ae779d97b36b7
Alice Saito <saito.alice@mdpr.jp>
2019-12-18 12:32:15 +0900

    ソースファイル追加

---
+++ iOS/Calendar_Alice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calendar_Alice
+//
+//  Created by 斉藤 アリス on 2019/12/17.
+//  Copyright © 2019 斉藤 アリス. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
