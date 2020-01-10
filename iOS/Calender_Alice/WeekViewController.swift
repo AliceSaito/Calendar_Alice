e3c01eb9ae7163fe452d7285d6a79dab23c2acc9
Alice Saito <saito.alice@mdpr.jp>
2020-01-10 14:09:00 +0900

    Tab Bar Controllerの追加

--- iOS/Calender_Alice/WeekViewController.swift
+++ iOS/Calender_Alice/WeekViewController.swift
@@ -2,31 +2,43 @@
 //  WeekViewController.swift
 //  Calender_Alice
 //
-//  Created by 斉藤 アリス on 2020/01/09.
-//  Copyright © 2020 斉藤 アリス. All rights reserved.
+//  Created by 斉藤 アリス on 2019/12/18.
+//  Copyright © 2019 斉藤 アリス. All rights reserved.
 //
 
 import UIKit
 
-class WeekViewController: UIViewController {
+class WeekViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+// 月表示のカレンダーを２回繰り返し表示する
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return 2
+        }
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //        ↓　items.countにしたら、itemsで書いたstringが表示される。文字でも数字でもOK。
+    //        return items.count
+    //        ↓ 0〜30までの数字を、順番に31個表示する。
+            return 31
+        }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeekCollectionViewCell", for: indexPath) as! WeekCollectionViewCell
+            
+    //        cell.myLabel.text = items[indexPath.item]
+            print(indexPath)
+    //indexPathは、itemとsectionと一緒に使う。itemは月表示のカレンダー、sectionはその繰り返しの回数。
+            cell.myLabel.text = "\(indexPath.section+1)-\(indexPath.item+1)"
+            return cell
+        }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+           print(indexPath.item)
+       }
 }
-
 
