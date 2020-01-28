0e7d2991c373cad15a6972f3ee997e2e83ffd567
Alice Saito <saito.alice@mdpr.jp>
2020-01-28 16:29:29 +0900

    ①休日を赤色で表記。②アプリを開いた時に、今月のカレンダーが表示される設定。

--- iOS/Calendar_Alice/BasicScreen/WeekViewController.swift
+++ iOS/Calendar_Alice/BasicScreen/WeekViewController.swift
@@ -8,7 +8,6 @@
 
 
 
-
 // 下記はDateComponentsの練習用コード
 //        // Dateオブジェクトを操作するクラス
 //        let calendar = Calendar(identifier: .gregorian)
@@ -51,107 +50,109 @@
 import UIKit
 
 class WeekViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    //storyboardのCollectionViewとコードを繋げた。
+    @IBOutlet weak var collectionView: UICollectionView!
     
-// 200年間の(年、Month)データ。200 x 12 = 2400件の月データがこのmonthArr配列に入っている。
-//staticが付いていると、別のクラスでも、クラス名.monthArrで直接呼び出せる
-static let monthArr: [MonthInfo] = {
     
-    // 200年間
-    var temp = [MonthInfo]()
-    (1900 ..< 2100).forEach { year in
-        // １年は12ヶ月
-        (1 ..< 13).forEach { month in
-            let monthInfo = MonthInfo.init(year: year, month: month, day: nil)
-            temp.append(monthInfo)
+    // 200年間の(年、Month)データ。200 x 12 = 2400件の月データがこのmonthArr配列に入っている。
+    //staticが付いていると、別のクラスでも、クラス名.monthArrで直接呼び出せる
+    static let monthArr: [MonthInfo] = {
+        
+        // 200年間
+        var temp = [MonthInfo]()
+        (1900 ..< 2100).forEach { year in
+            // １年は12ヶ月
+            (1 ..< 13).forEach { month in
+                let monthInfo = MonthInfo.init(year: year, month: month, day: nil)
+                temp.append(monthInfo)
+            }
         }
+        return temp
+    }()
+    
+    //★下記は年間カレンダーで使用する部分。あとで書く。
+    // $0はクロージャー。Arrayで使う時は全ての要素にアクセスするという意味。
+    //func groupForYearArr() -> [Int] {
+    //    return (1900 ..< 2100).map({ $0 })
+    //}
+    
+    
+    struct MonthInfo {
+        var year: Int
+        var month: Int
+        var day:Int?
     }
-    return temp
-}()
-
-//★下記は年間カレンダーで使用する部分。あとで書く。
-// $0はクロージャー。Arrayで使う時は全ての要素にアクセスするという意味。
-//func groupForYearArr() -> [Int] {
-//    return (1900 ..< 2100).map({ $0 })
-//}
-
-
-struct MonthInfo {
-    var year: Int
-    var month: Int
-    var day:Int?
-}
-
-//年と月をパラメータとして入れると、その年月の日付情報が返される
-func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
-
-// func getMonthDaysをタプルで書くと下記の通り。
-//略式はfunc getMonthDays(monthInfo: (Int,Int))。
-//func getMonthDays(monthInfo:(year: Int, month: Int)) -> [(year: Int, month: Int, day: Int)?] {
-
-    // Calendarは、アップルが提供しているFramework。
-    let calendar = Calendar.current
-
-    var dates: [MonthInfo?] = []
-//    上記を略さずに書くと下記の通り
-//    var dates: Array<MonthInfo?> = []
-// 上記をタプルで書くと下記の通り。
-//    var dates: [(year: Int, month: Int, day: Int)?] = []
-
-//DateComponentsの定義
-    let dateComponents = DateComponents(calendar: calendar, year: monthInfo.year, month: monthInfo.month, day: 1)
- 
-//カレンダー形式に変換
-    let startDay = calendar.date(from: dateComponents)!
-
-// !でunwrapをしない時はguardかifでOptional Binding
-// guard let startDay = composedDate else { return [] }
-//if let startDay = composedDate {}
     
-    // 月の１日目の曜日を特定する。for文で回して１日より前のマスにnilを入れる。
-    let component = calendar.component(Calendar.Component.weekday, from: startDay)
-    let weekday = component - 1
-
-    (0 ..< weekday).forEach { _ in
-        dates.append(nil)
+    //年と月をパラメータとして入れると、その年月の日付情報が返される
+    func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
+        
+        // func getMonthDaysをタプルで書くと下記の通り。
+        //略式はfunc getMonthDays(monthInfo: (Int,Int))。
+        //func getMonthDays(monthInfo:(year: Int, month: Int)) -> [(year: Int, month: Int, day: Int)?] {
+        
+        // Calendarは、アップルが提供しているFramework。
+        let calendar = Calendar.current
+        
+        var dates: [MonthInfo?] = []
+        //    上記を略さずに書くと下記の通り
+        //    var dates: Array<MonthInfo?> = []
+        // 上記をタプルで書くと下記の通り。
+        //    var dates: [(year: Int, month: Int, day: Int)?] = []
+        
+        //DateComponentsの定義
+        let dateComponents = DateComponents(calendar: calendar, year: monthInfo.year, month: monthInfo.month, day: 1)
+        
+        //カレンダー形式に変換
+        let startDay = calendar.date(from: dateComponents)!
+        
+        // !でunwrapをしない時はguardかifでOptional Binding
+        // guard let startDay = composedDate else { return [] }
+        //if let startDay = composedDate {}
+        
+        // 月の１日目の曜日を特定する。for文で回して１日より前のマスにnilを入れる。
+        let component = calendar.component(Calendar.Component.weekday, from: startDay)
+        let weekday = component - 1
+        (0 ..< weekday).forEach { _ in
+            dates.append(nil)
+        }
+        
+        //CollectionViewのCell(7×5)に順番に数字を入れていく
+        let totalDays = calendar.range(of: .day, in: .month, for: startDay)?.count ?? 0
+        (1 ..< totalDays + 1).forEach { day in
+            let data = MonthInfo.init(year: monthInfo.year, month: monthInfo.month, day: day)
+            // data.year
+            // print(data.year)
+            dates.append(data)
+        }
+        return dates
+        // nil, nil, nil, (2020, 1, 1), (2020, 1, 2), (2020, 1, 3), ...
     }
-
-//CollectionViewのCell(7×5)に順番に数字を入れていく
-    let totalDays = calendar.range(of: .day, in: .month, for: startDay)?.count ?? 0
-    (1 ..< totalDays + 1).forEach { day in
-         let data = MonthInfo.init(year: monthInfo.year, month: monthInfo.month, day: day)
-        // data.year
-        // print(data.year)
-        dates.append(data)
-    }
-    return dates
-    // nil, nil, nil, (2020, 1, 1), (2020, 1, 2), (2020, 1, 3), ...
-}
     
     
     
-    ///宿題の部分。yearMonthをmonthInfoに変える。tupleを使わない。
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-
+        
         scrollToDate()
     }
-
-
-
+    //アプリを開いた時に、今日の日付が開かれるようにするコード。
     private func scrollToDate(date: Date = Date()) {
-
+        let calendar = Calendar(identifier: .gregorian)
+        //dateの情報をdateIntervalで秒単位に変換する。→ それをdateComponentsでIntに変換して、今を表示する。
         let yearInterval = calendar.dateInterval(of: .year, for: date)!
         let monthInterval = calendar.dateInterval(of: .month, for: date)!
+        //Int?なのでguardでオプショナルバインディングをしている。
         guard let year = calendar.dateComponents([.year], from: yearInterval.start).year,
             let month = calendar.dateComponents([.month], from: monthInterval.start).month
-        else { return }
-
-        let yearMonth = (year, month)
-
-        for (section, value) in monthArr.enumerated() {
-            if yearMonth == value {
-
+            else { return }
+        
+        //for文で全ての月のデータが入っているArrayを回して、一致するデータを探す。
+        for (section, value) in WeekViewController.monthArr.enumerated() {
+            if year == value.year && month == value.month{
+                
                 let indexPath = IndexPath.init(row: 0, section: section)
+                
+                //該当の月のカレンダーが、常に画面の真ん中に現れるように設定。animatedは日付を検索した時に、falseだと直移動、trueだと高速スクロールで移動する。
                 collectionView.scrollToItem(at: indexPath, at: UICollectionView.ScrollPosition.top, animated: false)
                 return
             }
@@ -161,20 +162,20 @@ func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
     
     
     
-
+    
     // 月表示のカレンダーを繰り返し表示する
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return WeekViewController.monthArr.count
     }
     
-//全データを取得。2400月データがmonthArr配列に入っている。
-//numberOfItemsInSectionは外部パラメーター（ラベル）
+    //全データを取得。2400月データがmonthArr配列に入っている。
+    //numberOfItemsInSectionは外部パラメーター（ラベル）
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let yearMonth = WeekViewController.monthArr[section]
         let monthDays = getMonthDays(monthInfo: yearMonth)
         return monthDays.count
     }
-
+    
     //空のcellを用意する。storyboardで作った"WeekCollectionViewCell"と紐付ける。
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeekCollectionViewCell", for: indexPath) as! WeekCollectionViewCell
@@ -184,13 +185,23 @@ func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
         //myLabelはstoryboardで、私が決めたセルの名前。
         let yearMonth = WeekViewController.monthArr[indexPath.section]
         if let day = getMonthDays(monthInfo: yearMonth)[indexPath.item] {
+            //日曜日を赤色にする。7で割った時に余りが0になるのが日曜日。
+            if (indexPath.row % 7 == 0) {
+                cell.myLabel.textColor = UIColor.red
+                //　土曜日を青色にする。7で割った時に余りが6になるのが日曜日。
+            }else if (indexPath.row % 7 == 6 ) {
+                cell.myLabel.textColor = UIColor.blue
+            }else {
+                cell.myLabel.textColor = UIColor.black
+            }
             cell.myLabel.text = "\(day.day!)"
+            
         } else {
             //値のない場合は、セルは空欄にする。
             cell.myLabel.text = ""
-    }
-        return cell
         }
+        return cell
+    }
     
     //セルをタップした時に呼び出されるファンクション。これを使って予定表を作る。
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -199,7 +210,7 @@ func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
     
     //headerを設定するための戻り値を設定。
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-
+        
         //年月を表示するためのheaderを設定。
         var headerView = UICollectionReusableView()
         let width = self.view.frame.width
@@ -209,7 +220,7 @@ func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
             headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderViews", for: indexPath)
         }
         let yearMonth = WeekViewController.monthArr[indexPath.section]
-
+        
         //headerの上に年月を表示するラベルを作成。
         let label = UILabel()
         label.frame = CGRect.init(x: 0, y: 0, width: width, height: 40)
@@ -218,7 +229,7 @@ func getMonthDays(monthInfo:MonthInfo) -> [MonthInfo?] {
         _ = headerView.subviews.map({ $0.removeFromSuperview() })
         headerView.addSubview(label)
         return headerView
-
+        
     }
-
+    
 }
---
+++ iOS/Calendar_Alice/HolidayColor.swift
@@ -0,0 +1,10 @@
+//
+//  HolidayColor.swift
+//  Calendar_Alice
+//
+//  Created by 斉藤アリス on 2020/01/28.
+//  Copyright © 2020 斉藤 アリス. All rights reserved.
+//
+
+import Foundation
+
