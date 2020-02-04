3348a2dcb7a7983255e8d99a819acec0a97adad7
Alice Saito <saito.alice@mdpr.jp>
2020-02-04 13:43:09 +0900

    AddScheduleViewControllerの追加

---
+++ iOS/Calendar_Alice/BasicScreen/AddSchdule/ScheduleListViewCotroller.swift
@@ -0,0 +1,30 @@
+//
+//  ScheduleListViewCotroller.swift
+//  Calendar_Alice
+//
+//  Created by 斉藤アリス on 2020/02/04.
+//  Copyright © 2020 斉藤 アリス. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleListViewCotroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
