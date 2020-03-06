38a8746529ef36f8f5075a82ed3b90d82f09a76f
Alice Saito <saito.alice@mdpr.jp>
2020-03-06 16:26:03 +0900

    ScrollDayCollectionViewCell

--- iOS/Calendar_Alice/BasicScreen/ScrollDayCollectionViewCell.swift
+++ iOS/Calendar_Alice/BasicScreen/ScrollDayCollectionViewCell.swift
@@ -10,4 +10,15 @@ import UIKit
 class ScrollDayCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var number: UILabel!
+    @IBOutlet weak var dayOfWeekLabel: UILabel!
+    
+    
+    func setData(dayOfWeek:MonthInfo){
+        
+        if let dayOptinal = dayOfWeek.day {
+          
+            let str: String = String(dayOptinal)
+            self.number.text = str
+        }
+    }
 }
